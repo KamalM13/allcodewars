22ff224862af75f285ee2e8d0e86023deff6fc2b
KamalM13 <92383290+KamalM13@users.noreply.github.com>
2023-07-14 02:13:15 +0300

    Create Explosive Sum.py
    problem link: https://www.codewars.com/kata/52ec24228a515e620b0005ef

---
+++ Explosive Sum.py
@@ -0,0 +1,9 @@
+def exp_sum(n):
+    number = []
+    number.append(1)
+    for i in range(n) : number.append(0) 
+    for i in range(0,n+1):
+        for j in range(i,n+1):
+            number[j] += number[j-i]
+    return number[n]/2
+  
